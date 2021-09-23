ccbd97b34b06b9043220fd0a4ce3f375657027fa
Damian Lopez <elilopez10@live.com>
2021-09-22 19:01:11 -0600

    initial project

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  # api endpoints 
+  # all routes in the api namespace, 
+  # allow rails and react to communicate to each other
+  # no more nav route 
+  namespace :api do
+    
+    resources :todos do
+      resources :comments
+    end
+
+  end
+
+end
